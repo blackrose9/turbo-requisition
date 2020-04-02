6420a3801f76fc5c482a749f0840dd7e397c677b
blackrose9 <sarah.mwendwa@gmail.com>
2020-04-03 02:30:46 +0300

    Initial commit with basic xml set up

---
+++ app/src/main/java/com/example/easyrequisition/PlaceOrderActivity.java
@@ -0,0 +1,14 @@
+package com.example.easyrequisition;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class PlaceOrderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_order);
+    }
+}
