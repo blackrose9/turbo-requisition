6a56935cb2c0c7660d517f5192988744fe9c6bba
blackrose9 <sarah.mwendwa@gmail.com>
2020-04-03 13:13:39 +0300

    bind views with butterknife change action bar theme to noActionbar so I can customize my own

--- app/src/main/java/com/example/easyrequisition/MainActivity.java
+++ app/src/main/java/com/example/easyrequisition/MainActivity.java
@@ -1,14 +1,38 @@
 package com.example.easyrequisition;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.newOrderFab)
+    FloatingActionButton mNewOrderFab;
+    @BindView(R.id.orderRecyclerView)
+    RecyclerView mOrderRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        mNewOrderFab.bringToFront();
+        mNewOrderFab.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mNewOrderFab) {
+            Intent intent = new Intent(MainActivity.this, PlaceOrderActivity.class);
+            startActivity(intent);
+        }
     }
 }
--- app/src/main/java/com/example/easyrequisition/PlaceOrderActivity.java
+++ app/src/main/java/com/example/easyrequisition/PlaceOrderActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class PlaceOrderActivity extends AppCompatActivity implements View.OnClickListener {
     @BindView(R.id.addCustomerName)
@@ -22,6 +23,7 @@ public class PlaceOrderActivity extends AppCompatActivity implements View.OnClic
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_order);
+        ButterKnife.bind(this);
     }
 
     @Override
