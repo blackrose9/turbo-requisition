7b451f61ec6c75099d48bb775b9efdf8ebb14602
blackrose9 <sarah.mwendwa@gmail.com>
2020-04-03 13:04:37 +0300

    add firebase google json file bind views to place order activity add internet request to manifest

--- app/src/main/java/com/example/easyrequisition/PlaceOrderActivity.java
+++ app/src/main/java/com/example/easyrequisition/PlaceOrderActivity.java
@@ -1,14 +1,37 @@
 package com.example.easyrequisition;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class PlaceOrderActivity extends AppCompatActivity {
+import butterknife.BindView;
+
+public class PlaceOrderActivity extends AppCompatActivity implements View.OnClickListener {
+    @BindView(R.id.addCustomerName)
+    EditText mAddCustomerName;
+    @BindView(R.id.addOrderContent)
+    EditText mAddOrderContent;
+    @BindView(R.id.placeOrderBtn)
+    Button mPlaceOrderBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_place_order);
+    }
+
+    @Override
+    public void onClick(View v) {
+        if (v == mPlaceOrderBtn) {
+            Toast.makeText(PlaceOrderActivity.this, "Submitted", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void saveOrderToFirebase(String customerName, String orderContent) {
+//        mEntryReference.push().setValue(new Order(title, entry));
     }
 }
